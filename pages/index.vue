830f414f0c6aeecd46ef508eb01526dce64d2b84
mcolo <max.miracolo@gmail.com>
2021-02-10 15:32:35 -0500

    remove logo and update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        movieclub-nuxt
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <input type="text" name="search" :value="search" @keyup="autocomplete" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    autocomplete() {
+      alert(this.search)
+    }
+  }
+}
 </script>
 
 <style>
@@ -39,35 +27,5 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
