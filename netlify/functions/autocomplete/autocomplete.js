b5c1da72a8a0053e72e6a1434fd4ce3af2ea4ffd
mcolo <max.miracolo@gmail.com>
2021-02-10 15:33:19 -0500

    add serverless function

---
+++ netlify/functions/autocomplete/autocomplete.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+exports.handler = async function(event, context) {
+  // your server-side functionality
+  if (!event.body.query) return
+
+  try {
+    const trie = fs.readFileSync('./autocomplete_trie.json', 'utf8')
+    const ids = suggestions(event.body.query)
+
+    if (ids) {
+      const movieData = getMovieData(ids)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(movieData)
+      }
+    } else {
+      return {
+        statusCode: 404,
+        body: 'Failed to find movie data'
+      }
+    }
+  } catch (err) {
+    console.log(`Error reading file from disk: ${err}`)
+  }
+}
+
+function suggestions(str) {
+  let response = []
+  str = str.toLowerCase()
+  try {
+    const startNode = str.split('').reduce((node, char) => {
+      return node[char]
+    }, trie)
+    response = getIds(startNode)
+    return response
+  } catch (err) {
+    console.log(err)
+    return null
+  }
+}
+
+function getIds(obj, ids = []) {
+  if (obj['ids']) {
+    ids.push(...obj['ids'])
+  }
+
+  for (let prop in obj) {
+    if (prop !== 'ids') {
+      getIds(obj[prop], ids)
+    }
+  }
+  return ids
+}
+
+function getMovieData(ids) {
+  let data = []
+  ids.forEach((id, index) => {
+    if (movieData[id].r) {
+      data.push({
+        title: movieData[id].t,
+        year: movieData[id].y,
+        rating: movieData[id].r,
+        id
+      })
+    }
+  })
+  // data.sort( (a,b) => b.rating - a.rating);
+  data.splice(9, data.length - 10)
+  return data
+}
